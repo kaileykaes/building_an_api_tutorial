7ce2c007a796aa00ce551cd0c3da9bd296349ccf
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-20 15:04:25 -0400

    set up test & faker

---
+++ spec/requests/api/v1/books_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Books API' do 
+  it 'sends a list of books' do 
+    create_list(:book, 3)
+    get '/api/v1/books'
+    expect(response).to be_successful
+  end
+end
